685aa46ba1bcf227f02238955fb76cb242afc48b
vedem1192 <veronique.demers@polymtl.ca>
2018-06-20 22:09:13 -0400

    project start + ml models

---
+++ MLTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLTest
+//
+//  Created by veronique demers on 18-06-20.
+//  Copyright © 2018 vedem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
